ebfd847add208096ab4616540c01a8020fdb6301
Afaf Zakhya <pbaffiliate4@gmail.com>
2018-09-24 10:57:01 +0300

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,16 +2,13 @@
 <html>
   <body>
 
-<h3>JavaScript Comments are NOT Executed at all</h3>
-
+<h1>JavaScript Comments are NOT Executed</h1>
 <p id="demo1"></p>
-
 <script>
-var v;
-v = 7;
-// v = 9; I will not be executed
-document.getElementById("demo2").innerHTML = v;
+var f;
+f = 7;
+// f = 9; I will not be executed
+document.getElementById("demo2").innerHTML = f;
 </script>
-
 </body>
 </html>
